7019bd4fdc6e21fe1d743075e849da4aada2dddf
Danil Bezrukov <101512922+DanilBezrukov@users.noreply.github.com>
2023-04-04 18:53:32 +0300

    Update functionConvertsKeysToUpperCase.js

--- functionConvertsKeysToUpperCase.js
+++ functionConvertsKeysToUpperCase.js
@@ -1,38 +1,44 @@
 function getObjKeysToUpperCase(obj) {
-    // проверяем является ли передаваемый аргумент объектом
-    if (typeof obj !== 'object') return obj;
+  // console.log(Array.isArray(obj) , obj)
+  // проверяем является ли передаваемый аргумент объектом 
+  if (typeof obj !== 'object' || obj === null) return obj;
+  // если передаваемый аргумент массив то рекурсией проверяем его значения
+  if (Array.isArray(obj)) {
+    return obj.map(getObjKeysToUpperCase);
+  }
+  // получаем ключи объекта
+  const keys = Object.keys(obj);
 
-    // получаем ключи объекта
-    const keys = Object.keys(obj);
-    const result = {};
+  // перебираем массив из ключей
+  for (const key of keys) {
+    // Сохраняем изначальное значение
+    const value = obj[key]
+    // возводим каждый ключ в верхний регистор
+    const upperCaseKey = key.toUpperCase();
+    //удаляем изначальный ключ
+    delete obj[key]
+    // вместо удаленного добавляем ключ в верхнем регистре и присваиваем ранее сохраненное значение
+    // и проверяем вложенность за счет рекурсии
+    obj[upperCaseKey] = getObjKeysToUpperCase(value);
+  }
+  return obj;
+}
 
-    // перебираем массив из ключей
-    for (const key of keys) {
-        // возводим каждый ключ в верхний регистор
-      const upperCaseKey = key.toUpperCase();
-
-      // Присваиваем ключу уже в верхнем регистре его изначальное значение
-      // За счет рекурсии проверяем вложенность
-      result[upperCaseKey] = getObjKeysToUpperCase(obj[key]);
+const testObj = {
+  level1_1: {
+    level2_1: [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+    level2_2: {
+      level3_1: "string",
+      level3_2: [
+        { level4_1: 1 },
+        { level4_2: true },
+        { level4_3: 1 },
+      ]
     }
-    return result;
-  }
+  },
+  level1_2: "value3"
+}
 
-  const testObj = {
-    level1_1 : {
-        level2_1 : [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-        level2_2 : {
-            level3_1 : "string",
-            level3_2 : [
-                {level4_1 : 1},
-                {level4_2 : true},
-                {level4_3 : 1},
-            ]
-        }
-    },
-    level1_2 : "value3"
-  }
+const result = getObjKeysToUpperCase(testObj)
+console.log(result)
 
-  const result = getObjKeysToUpperCase(testObj)
-  console.log(result)
-  
